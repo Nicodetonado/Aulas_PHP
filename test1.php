e6025087c1a48c61c5700403963206e2c20d57b6
Nicodetonado <101210605+Nicodetonado@users.noreply.github.com>
2023-08-16 20:22:31 -0300

    16-08
    aula 16-08

---
+++ test1.php
@@ -0,0 +1,14 @@
+<?php
+
+	print "Calcule a soma dos valores <br/> <br/>";
+
+	$v1 = 5;
+	$v2 = 10;
+	$vsoma = $v1 + $v2;
+
+	printf("A soma do primeiro valor: %.2f, e do segundo valor: %.2f é: <br/>", $v1, $v2);
+	print ($vsoma);
+
+	print $v1. "-" .$v2. "-" .$vsoma;
+
+?>
